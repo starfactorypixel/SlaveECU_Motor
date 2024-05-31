bca9c2461a04e67ec59425406c4ad21830d2854d
GrKoR <dr.g.kornev@gmail.com>
2024-05-31 23:51:23 +0300

    fix: board_ver++, board_v1 url in readme

--- include/About.h
+++ include/About.h
@@ -5,7 +5,7 @@ namespace About
 	static constexpr char name[] = "MotorECU";
 	static constexpr char desc[] = "Motor control board for Pixel project";
 	static constexpr char board_type = Consts::BOARD_TYPE_MOTORS1;		// 5 bits
-	static constexpr char board_ver = 1;		// 3 bits
+	static constexpr char board_ver = 2;		// 3 bits
 	static constexpr char soft_ver = 1;			// 6 bits
 	static constexpr char can_ver = 1;			// 2 bits
 	static constexpr char git[] = "https://github.com/starfactorypixel/SlaveECU_Motor";
